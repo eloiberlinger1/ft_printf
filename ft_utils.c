edec5382f4a0def438c81aecafa49c6fb1cc9ab7
Eloi Berlinger <eloi.berling@protonmail.com>
2025-10-26 09:50:15 +0100

    Handling %

--- ft_utils.c
+++ ft_utils.c
@@ -6,17 +6,14 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 12:25:32 by eberling          #+#    #+#             */
-/*   Updated: 2025/10/26 09:40:03 by eberling         ###   ########.fr       */
+/*   Updated: 2025/10/26 09:50:01 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	process_char(va_list args)
+static int	process_char(int value)
 {
-	char	value;
-
-	value = (char)va_arg(args, int);
 	return (write(1, &value, 1));
 }
 
@@ -34,25 +31,29 @@ static int	process_str(va_list args)
 
 static int	process_int(int value)
 {
-	char 	*ret;
+	char 	*temp;
+	int		ret;
 	
-	ret = ft_itoa(value);
-	if(!ret)
+	temp = ft_itoa(value);
+	if(!temp)
 		return (0);
-	ft_putstr_fd(ret, 1);
-	free(ret);
-	return (1);
+	ft_putstr_fd(temp, 1);
+	ret = ft_strlen(temp);
+	free(temp);
+	return (ret);
 }
 
 int	process(char s, va_list args)
 {
-	//char keys[] = "cspdiuxX%";
+	//char keys[] = "pxX%";
 
 	if (s == 'c')
-		return (process_char(args));
+		return (process_char(va_arg(args, int)));
 	else if(s == 's')
 		return (process_str(args));
 	else if(s == 'd' || s == 'i' || s == 'u')
 		return (process_int(va_arg(args, int)));
+	else if(s == '%')
+		return (process_char('%'));
 	return (0);
 }
--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 int main()
 {
     printf("\nft_printf :\n");
-    ft_printf("0000%i0000", -5185165);
+    ft_printf("0000%%%%0000");
     printf("\n\nprintf :\n");
-    printf("0000%i0000", -5185165);
+    printf("0000%%%%0000");
     printf("\n\n");
 
     return 1;
